5936d99bf0256826b5e347f4d095e6c541515506
Anastasiia-Khab <anastasykhab@gmail.com>
2018-07-07 02:57:30 +0300

    Changed base class. Added methods get_commits_by hash, get_commit_by_hash, get_contributors

--- request_sender/request_sender_base.py
+++ request_sender/request_sender_base.py
@@ -18,7 +18,7 @@ class RequestSender:
     def get_repo(self):
         """
         Gets repository info in JSON format
-        ex:
+        example:
         {
             "id": "unique id",
             "repo_name": "repository name",
@@ -33,7 +33,7 @@ class RequestSender:
     def get_branches(self):
         """
         Gets information about branches in JSON format
-        ex:
+        example:
         [
             {
                 "name": "branch name"
@@ -48,7 +48,7 @@ class RequestSender:
     def get_commits(self):
         """
         Gets information about commits in JSON format
-        ex
+        example:
         [
             {
                 "hash": "commit hash",
@@ -62,3 +62,55 @@ class RequestSender:
 
         :return: string - JSON formatted response
         """
+
+    def get_commits_by_branch(self, branch_name):
+        """
+                Gets information about commits (in branch specified) in JSON format
+        example:
+        [
+            {
+                "hash": "commit hash",
+                "author": "commit author",
+                "message": "commit message",
+                "date": "date when committed"
+
+            },
+            ...
+        ]
+
+        :param branch_name: string
+        :return: string - JSON formatted response
+        """
+
+    def get_commit_by_hash(self, hash_of_commit):
+        """
+        Gets information about the commit by hash in JSON format
+        example:
+        {
+            "hash": "commit hash",
+            "author": "commit author",
+            "message": "commit message",
+            "date": "date when committed"
+
+        }
+
+        :param hash_of_commit: string
+        :return: string - JSON formatted response
+        """
+
+    def get_contributors(self):
+        """
+         Gets information about contributors
+           example:
+           [
+                {
+                    "name": "contributor name",
+                    "number_of_commits": "number of commits",
+                    "email": "contributor email",
+                    "url": "contributor url"
+                },
+                ...
+            ]
+
+            :return: string - JSON formatted response
+         """
