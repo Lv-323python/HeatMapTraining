988b6bae3f49291400344daa4a9d5b09dbcaa86f
mixa1901 <myskiv1997@gmail.com>
2018-07-15 11:05:38 +0300

    Created test/githab_mixa1901

---
+++ tests/github_test_request_sender.py
@@ -0,0 +1,23 @@
+"""
+Contains TestsRequestSenderGitHub class that provides methods for testing
+of GitHubRequestSender class which sends API requests to GitHub
+"""
+
+
+class TestsRequestSenderGitHub:
+    """
+    Provides pytests for testing RequestSenderBase methods realized by BitbucketRequestSender
+    """
+    def test_function(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def test_function_2(self):
+        """
+
+        :return:
+        """
+        pass
