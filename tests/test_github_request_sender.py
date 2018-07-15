6e58d958b784c2ffbddd0c8b5b422029e396a7c4
mixa1901 <myskiv1997@gmail.com>
2018-07-15 16:01:46 +0300

    Added functions to test_github(test_get_commits and  test_get_commit_by_hash

--- tests/test_github_request_sender.py
+++ tests/test_github_request_sender.py
@@ -3,26 +3,45 @@ Contains TestsRequestSenderGitHub class that provides methods for testing
 of GitHubRequestSender class which sends API requests to GitHub
 """
 
-from heat_map_training.request_sender import github_request_sender
+from heat_map_training.request_sender.github_request_sender import GithubRequestSender
 
 
-a = github_request_sender.GithubRequestSender('Lv-323python', 'HeatMapTraining')
-print(a.get_contributors())
-
-class TestsRequestSenderGitHub:
+class TestsGithubRequestSender:
     """
-    Provides pytests for testing RequestSenderBase methods realized by BitbucketRequestSender
+    Provides pytests for testing RequestSenderBase methods realized by GithubRequestSender
     """
-    def test_function(self):
-        """
 
-        :return:
+    def test_get_commits(self):
         """
-        pass
+        Unittest function for testing method get_commits in GithubRequestSender
+        """
+        assert GithubRequestSender('mixa1901', 'test').get_commits() == [
+            {
+                'hash': '5a8a11fa7b0fc08d59e0fd7c435c3073459ae87a',
+                'author': 'mixa1901',
+                'message': 'test2',
+                'date': 1531637855
+            },
+            {
+                'hash': 'e9298428816b7419afd11af2397f7f3745f727d9',
+                'author': 'mixa1901',
+                'message': 'first commit',
+                'date': 1531637760
+            }
+        ]
 
-    def test_function_2(self):
-        """
+        assert GithubRequestSender('unknown', 'unknown').get_commits() is None
 
-        :return:
+    def test_get_commit_by_hash(self):
         """
-        pass
+        Unittest function for testing method get_commit_by_hash in GithubRequestSender
+        """
+        assert GithubRequestSender('mixa1901', 'test').get_commit_by_hash(
+            '5a8a11fa7b0fc08d59e0fd7c435c3073459ae87a') == {
+                   'hash': '5a8a11fa7b0fc08d59e0fd7c435c3073459ae87a',
+                   'author': 'mixa1901',
+                   'message': 'test2',
+                   'date': 1531637855
+               }
+
+        assert GithubRequestSender('unknown', 'unknown').get_commit_by_hash('unknown') is None
