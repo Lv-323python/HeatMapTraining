c41a6545ab1f4f9c67d04c9fc128568541701b64
BoartK <boartmain@gmail.com>
2018-07-26 05:08:36 +0300

    Changed inheritance

--- heat_map_training/request_sender/gitlab_v3_request_sender_base.py
+++ heat_map_training/request_sender/gitlab_v3_request_sender_base.py
@@ -4,99 +4,27 @@ to web-based hosting services for version control using Git
 """
 
 import requests
-from heat_map_training.request_sender.request_sender_base import \
-    RequestSender  # pylint: disable=import-error
+from heat_map_training.request_sender.gitlab_request_sender import \
+    GitLabRequestSender  # pylint: disable=import-error
 from heat_map_training.utils.helper import format_date_to_int
 from heat_map_training.utils.request_status_codes import STATUS_CODE_OK
 
-TOKEN = ""
-ENDPOINT = "?private_token=" + TOKEN
+TOKEN = "?private_token=1SxVpxZE2536J4YyAmxp"
 
 
-class GitLabV3RequestSender(RequestSender):
+class GitLabV3RequestSender(GitLabRequestSender):
     """
         GitLab class that provides realisation for sending API requests
         to web-based hosting services for version control using Git
     """
 
     def __init__(self, owner, repo, base_url="http://boart-lenovo-ideapad-y510p/api/v3/projects/"):
-        RequestSender.__init__(
+        GitLabRequestSender.__init__(
             self,
             base_url=base_url,
             owner=owner,
-            repo=repo
-        )
-
-    def get_repo(self):
-        # get url of remote repository given as input
-        """
-        Takes repository name on GitLab and owner as parameters and
-        returns repository info in JSON format
-
-        :return: dictionary.
-        Example:
-        {
-            "id": "unique id",
-            "repo_name": "repository name",
-            "creation_date": "date",
-            "owner": "repository owner",
-            "url": "repository url"
-        }
-        """
-        url_repo = self.base_url + self.owner + "%2F" + self.repo + ENDPOINT
-
-        # get response and check it's validation
-        response = requests.get(url_repo)
-
-        if not response.status_code == STATUS_CODE_OK:
-            return None
-
-        # get json of repository
-        repo_info = response.json()
-
-        # retrieve only info about repository
-        repo = {
-            "id": repo_info["id"],
-            "repo_name": repo_info["name"],
-            "creation_date": format_date_to_int(repo_info["created_at"][:23],
-                                                "%Y-%m-%dT%H:%M:%S.%f"),
-            "owner": repo_info["path_with_namespace"].split("/")[0],
-            "url": repo_info["web_url"]
-        }
-
-        return repo
-
-    def get_branches(self):
-        """
-        get branches of given repository on GitLab
-
-        :return: list of dictionaries.
-        Example:
-        [
-            {
-                "name": "branch name"
-            },
-            ...
-        ]
-        """
-
-        # get url of remote repository given as input
-        url_branches = (self.base_url + self.owner + "%2F" + self.repo + "/repository/branches" +
-                        ENDPOINT)
-
-        # get response and check it's validation
-        response = requests.get(url_branches)
-
-        if not response.status_code == STATUS_CODE_OK:
-            return None
-
-        # get json of branches
-        branches_info = response.json()
-
-        # retrieve only info about name of the branches
-        branches = [{"name": branch["name"]} for branch in branches_info]
-
-        return branches
+            repo=repo)
+        self.token = TOKEN
 
     def _get_branch_for_commit(self, commit_hash):
 
@@ -105,7 +33,7 @@ class GitLabV3RequestSender(RequestSender):
         # ex (v4): /projects/:id/repository/commits/:sha/refs
 
         api_gitlab = (self.base_url + self.owner + "%2F" + self.repo + "/repository/commits/" +
-                      commit_hash + "/statuses" + ENDPOINT)
+                      commit_hash + "/statuses" + self.token)
         branch_info = requests.get(api_gitlab).json()
         try:
             return branch_info[0]['ref']
@@ -136,7 +64,7 @@ class GitLabV3RequestSender(RequestSender):
         """
         # get url of remote repository given as input
         url_commits = (self.base_url + self.owner + "%2F" + self.repo + "/repository/commits" +
-                       ENDPOINT)
+                       self.token)
 
         response = requests.get(url_commits)
 
@@ -157,46 +85,6 @@ class GitLabV3RequestSender(RequestSender):
         } for commit in commits_info]
 
         return commits
-
-    def get_contributors(self):
-        """
-        Takes repository name and owner as parameters and returns
-        information about contributors in JSON format
-
-        :return: list of dictionaries.
-        Example:
-        [
-            {
-                "name": "contributor name",
-                "number_of_commits": "number of commits",
-                "email": "contributor email",
-                "url": "contributor url"
-            },
-            ...
-        ]
-        """
-        # get url of remote repository given as input
-        url_contributors = (self.base_url + self.owner + "%2F" + self.repo +
-                            "/repository/contributors" + ENDPOINT)
-
-        # get response and check it's validation
-        response = requests.get(url_contributors)
-
-        if not response.status_code == STATUS_CODE_OK:
-            return None
-
-        # get json of contributors
-        contributors_info = response.json()
-
-        # retrieve only info about contributors
-        contributors = [{
-            "name": contributors_info[i]["name"],
-            "number_of_commits": contributors_info[i]["commits"],
-            "email": contributors_info[i]["email"],
-            "url": "None"  # to be continued...
-        } for i in range(len(contributors_info))]
-
-        return contributors
 
     def get_commit_by_hash(self, hash_of_commit):
 
@@ -219,7 +107,7 @@ class GitLabV3RequestSender(RequestSender):
         """
         # get url of remote repository given as input
         url_commit = (self.base_url + self.owner + "%2F" + self.repo +
-                      "/repository/commits/" + hash_of_commit + ENDPOINT)
+                      "/repository/commits/" + hash_of_commit + self.token)
 
         response = requests.get(url_commit)
 
@@ -262,7 +150,8 @@ class GitLabV3RequestSender(RequestSender):
         """
 
         api_commits_by_branch = (self.base_url + self.owner + "%2F" + self.repo +
-                                 "/repository/commits" + ENDPOINT + "&" + "ref_name=" + branch_name)
+                                 "/repository/commits" + self.token +
+                                 "&" + "ref_name=" + branch_name)
 
         # get response and check it's validation
         response = requests.get(api_commits_by_branch)
