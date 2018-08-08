00a6eaccf2ae7a98f7d6b9071d6c772d17f4b336
ihorss <res.ihor.ua@gmail.com>
2018-08-09 02:09:37 +0300

    General fix consumer module

--- consumer/receiver.py
+++ consumer/receiver.py
@@ -1,37 +1,36 @@
-'''
-    qqq
-'''
+"""
+    Consumes requests from provider(sender), sends result to provider(sender)
+"""
 from ast import literal_eval
 
 import pika
 
-from builder import Builder # pylint: disable=import-error
+from helper.builder import Builder  # pylint: disable=import-error
 
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
+CONNECTION = pika.BlockingConnection(pika.ConnectionParameters(
     host='172.17.0.2'))
-channel = connection.channel()
+CHANNEL = CONNECTION.channel()
 
-channel.queue_declare(queue='request')
-channel.queue_declare(queue='response')
+CHANNEL.queue_declare(queue='request')
+CHANNEL.queue_declare(queue='response')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 
 def worker(body):
-    '''
+    """
 
     :param body:
-    :return:
-    '''
-
+    :return: None, but sends result to provider(sender)
+    """
+    # decode request from provider(sender)
     body = literal_eval(body.decode())
     req_name = body['action']
     print('body in worker', body)
 
+    # gets response from API
     with Builder(body) as obj:
-        print(obj)
-
+        # declare object with methods
         request = {
             'get_repo': obj.get_repo,
             'get_branches': obj.get_branches,
@@ -49,27 +48,34 @@ def worker(body):
 
         print('response', response)
 
-        channel.basic_publish(exchange='',
+        # sends API response to provider(sender)
+        CHANNEL.basic_publish(exchange='',
                               routing_key='response',
                               body=str(response))
 
 
-def callback(ch, method, properties, body):
-    '''
-
-    :param ch:
-    :param method:
-    :param properties:
-    :param body:
+def callback(ch_c, method_m, properties_p, body):
+    """
+        Consumes request from provider(sender)
+    :param ch_c: unused param
+    :param method_m: unused param
+    :param properties_p: unused param
+    :param body: received message
     :return:
-    '''
+    """
+
+    # print unused params to pass pylit check
+    print(ch_c, method_m, properties_p)
 
     print(" [x] Received %r" % (body,))
+
+    # uses 'worker' function to get API response
+    # and send it to provider(sender)
     worker(body)
 
 
-channel.basic_consume(callback,
-                      queue='request',
-                      no_ack=True)
+# declare consuming
+CHANNEL.basic_consume(callback, queue='request', no_ack=True)
 
-channel.start_consuming()
+# start waiting for request from provider(sender)
+CHANNEL.start_consuming()
--- consumer/sender.py
+++ consumer/sender.py
@@ -1,64 +1,82 @@
-'''
-    qqqq
-'''
-from pprint import pprint
-from ast import literal_eval
+"""
+    Sends request, waits for response, returns response
+"""
 
 import pika
 
 
 def sender(body):
-    '''
+    """
 
-    :param body:
-    :return:
-    '''
+    :param body: str - string of params for request
+    :return response: str - response from consumer
+    """
 
+    # declare result
+    list_for_result = []
+
+    # start connection
     connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='172.17.0.2'))
     channel = connection.channel()
 
+    # declare queues
     channel.queue_declare(queue='request')
     channel.queue_declare(queue='response')
 
-    channel.basic_publish(exchange='',
-                          routing_key='request',
-                          body=body)
-    print(f" [x] Sent '{body}'")
+    # sends message to consumer
+    channel.basic_publish(exchange='', routing_key='request', body=body)
 
-    def callback(ch, method, properties, body):
-        '''
-
-        :param ch:
-        :param method:
-        :param properties:
-        :param body:
-        :return:
-        '''
+    def callback(a_a, b_b, c_c, body):
+        """
+            Base function for consuming
+        :param a_a: unused param
+        :param b_b: unused param
+        :param c_c: unused param
+        :param body: received message
+        :return: None, but save response from consumer to 'result'
+        """
+        # print unused params to pass pylit check
+        print(a_a, b_b, c_c)
 
         print(" [x] Received %r\n" % (body,))
-        result = literal_eval(body.decode())
-        pprint(result)
+
+        # stops waiting for response
         channel.stop_consuming()
 
-    channel.basic_consume(callback,
-                          queue='response',
-                          no_ack=True)
+        # save response from consumer to 'result'
+        list_for_result.append(body)
 
+    # declare consuming params
+    channel.basic_consume(callback, queue='response', no_ack=True)
+
+    # starts consuming(waiting for) response using 'callback' function
     channel.start_consuming()
     connection.close()
 
+    # gets response from list_for_result and clears list_for_result
+    result = list_for_result[0]
+    list_for_result.clear()
+    # returns result
+    return result
 
-EXAMPLE = "{'base_url':'https://api.bitbucket.org/2.0'," \
-          "'version':'2','git_client':'bitbucket'," \
-          "'token':None,'repo':'newtestrep','owner':" \
-          " 'ihorss','hash':None,'branch':" \
-          " None,'action': 'get_repo'}"
 
-while True:
-    MESSAGE = input(
-        ' [*] Waiting for input. To exit press CTRL+C\nInput params\n '
-        ' ex.{' + EXAMPLE + ':\n'
-                            '      *optional - branch_name or hash_of_commit\nINPUT: ')
-    sender(MESSAGE)
-    print('Request has been executed\n\n')
+# # Working example request message for bitbucket
+#
+# EXAMPLE = "{'base_url':'https://api.bitbucket.org/2.0'," \
+#           "'version':'2','git_client':'bitbucket'," \
+#           "'token':None,'repo':'newtestrep','owner':" \
+#           " 'ihorss','hash':None,'branch':" \
+#           " None,'action': 'get_repo'}"
+#
+# # for test without flask web server
+# from pprint import pprint
+# from ast import literal_eval
+#
+# while True:
+#     MESSAGE = input(
+#         ' [*] Waiting for input. To exit press CTRL+C\nInput params\n '
+#         ' ex.:\n   ' + EXAMPLE + '\n\nINPUT:\n ')
+#     response = sender(MESSAGE)
+#     pprint(literal_eval(response.decode()))
+#     print('Request has been executed\n\n')
