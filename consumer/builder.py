da116d665617f23d90111d6df9e65b0d0ae27ea0
anna <hannabretsko@gmail.com>
2018-08-08 18:22:14 +0300

    added bitbucket server version to builder

--- consumer/builder.py
+++ consumer/builder.py
@@ -1,33 +1,44 @@
-from heat_map_training.request_sender.bitbucket_request_sender import BitbucketRequestSender
+"""
+This module provides a builder that returns instance of provider class
+"""
+from heat_map_training.request_sender.bitbucket_request_sender \
+    import BitbucketRequestSender, BitbucketServerRequestSender
 from heat_map_training.request_sender.github_request_sender import GithubRequestSender
 from heat_map_training.request_sender.gitlab_request_sender import GitLabRequestSender
 from heat_map_training.request_sender.gitlab_v3_request_sender_base import GitLabV3RequestSender
 
-"""
-This is a class builder that returns instance of provider depending on its git_client
-"""
-
 
 class Builder:
-    def __init__(self, git_client, version, repo, owner, token=''):
-        self.git_client = git_client
-        self.version = version
-        self.repo = repo
-        self.owner = owner
-        self.token = token
+    """
+    This is a class builder that returns instance of provider depending on its git_client
+    """
 
-    def build_provider(self):
+    def __init__(self, request_dict):
+        self.git_client = request_dict['git_client']
+        self.version = request_dict['version']
+        self.repo = request_dict['repo']
+        self.owner = request_dict['owner']
+        self.token = request_dict['token']
+        self.provider = None
+
+    def __enter__(self):
         """
         This method is responsible for building provider with given methods.
         Note that GitHub provider takes token as a positional parameter
         :return: instance of provider class
         """
         if self.git_client == 'bitbucket':
-            return BitbucketRequestSender(self.repo, self.owner)
+            if self.version == '1':
+                self.provider = BitbucketServerRequestSender(self.repo, self.owner)
+            elif self.version == '2':
+                self.provider = BitbucketRequestSender(self.repo, self.owner)
         elif self.git_client == 'gitlab':
             if self.version == '3':
-                return GitLabV3RequestSender(self.repo, self.owner)
-            else:
-                return GitLabRequestSender(self.repo, self.owner)
+                self.provider = GitLabV3RequestSender(self.repo, self.owner)
+            self.provider = GitLabRequestSender(self.repo, self.owner)
         elif self.git_client == 'github':
-            return GithubRequestSender(self.repo, self.owner, self.token)
+            self.provider = GithubRequestSender(self.repo, self.owner, self.token)
+        return self.provider
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        del self.provider
